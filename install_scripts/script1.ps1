bbe9fe79b0e4d3d269699cdb0b5fa3166c60a324
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-05-04 11:56:24 +0200

    Update script1.ps1
    This should open the process in a powershell window even if launched from Max

--- install_scripts/script1.ps1
+++ install_scripts/script1.ps1
@@ -1,3 +1,7 @@
+﻿invoke-expression 'cmd /c start powershell -Command { New-Item -Path C:\Windows\Temp\MOZLib_setup_files -ItemType directory ; Import-Module BitsTransfer ; Start-BitsTransfer "https://wsldownload.azureedge.net/Ubuntu_2004.2020.424.0_x64.appx" "C:\Windows\Temp\MOZLib_setup_files\Ubuntu_2004.2020.424.0_x64.appx" ; Add-AppxPackage C:\Windows\Temp\MOZLib_setup_files\Ubuntu_2004.2020.424.0_x64.appx ; cd "C:\Program Files\WindowsApps\CanonicalGroupLimited.Ubuntu20.04onWindows_2004.2020.424.0_x64__79rhkp1fndgsc" ; ubuntu2004.exe ; Remove-Item -Path "C:\Windows\Temp\MOZLib_setup_log\step2.txt" -Force }'
+
+<#
+
 #Create a directory where to download all the needed stuff
 New-Item -Path C:\Windows\Temp\MOZLib_setup_files -ItemType directory
 
@@ -10,9 +14,11 @@ Add-AppxPackage C:\Windows\Temp\MOZLib_setup_files\Ubuntu_2004.2020.424.0_x64.ap
 
 
 #run Ubuntu
-#cd "C:\Program Files\WindowsApps\CanonicalGroupLimited.Ubuntu20.04onWindows_2004.2020.424.0_x64__79rhkp1fndgsc"
-#ubuntu2004.exe 
+cd "C:\Program Files\WindowsApps\CanonicalGroupLimited.Ubuntu20.04onWindows_2004.2020.424.0_x64__79rhkp1fndgsc"
+ubuntu2004.exe 
 #then set username and password
 
 #remove step2.txt
 Remove-Item -Path "C:\Windows\Temp\MOZLib_setup_log\step2.txt" -Force 
+
+#>
