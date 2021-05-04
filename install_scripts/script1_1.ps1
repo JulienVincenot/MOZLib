039653f6455f301fb6ea5b56fd7281b09d026e69
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-05-04 11:21:41 +0200

    Update script1_1.ps1

--- install_scripts/script1_1.ps1
+++ install_scripts/script1_1.ps1
@@ -1,3 +1,6 @@
-﻿cd "C:\Program Files\WindowsApps\CanonicalGroupLimited.Ubuntu20.04onWindows_2004.2020.424.0_x64__79rhkp1fndgsc"
-ubuntu2004.exe 
+﻿Start-Process PowerShell -ArgumentList 'cd "C:\Program Files\WindowsApps\CanonicalGroupLimited.Ubuntu20.04onWindows_2004.2020.424.0_x64__79rhkp1fndgsc" ; ubuntu2004.exe ; Read-Host "Press Enter" '
+
+
+#cd "C:\Program Files\WindowsApps\CanonicalGroupLimited.Ubuntu20.04onWindows_2004.2020.424.0_x64__79rhkp1fndgsc"
+#ubuntu2004.exe 
 #then set username and password
