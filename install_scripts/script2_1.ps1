f28f78f714a32f9400d168cdd1ccbffdcc31ecfc
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-05-04 12:59:10 +0200

    changed the download folder to MOZLib_setup_files

--- install_scripts/script2_1.ps1
+++ install_scripts/script2_1.ps1
@@ -1,6 +1,13 @@
-﻿$env:UserName #find the UserName
+﻿<#
+$env:UserName #find the UserName
 $client = new-object System.Net.WebClient
 $client.DownloadFile(“https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi”,“C:\Users\$env:UserName\Downloads\wsl_update_x64.msi”) #download the update
 "C:\Users\$env:UserName\Downloads\wsl_update_x64.msi" #run the update
 
+Read-Host -Prompt "Done!! Press Enter to exit and run script3"
+#>
+
+$client = new-object System.Net.WebClient
+$client.DownloadFile(“https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi”,“C:\Windows\Temp\MOZLib_setup_files\wsl_update_x64.msi”) #download the update
+“C:\Windows\Temp\MOZLib_setup_files\wsl_update_x64.msi”#run the update
 Read-Host -Prompt "Done!! Press Enter to exit and run script3"
