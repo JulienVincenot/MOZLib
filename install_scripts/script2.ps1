256cd0c80ac169ff26a65ffd7a9fc639fe777da5
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-06-13 10:43:01 +0200

    Update script2.ps1

--- install_scripts/script2.ps1
+++ install_scripts/script2.ps1
@@ -16,4 +16,4 @@ Get-WmiObject Win32_OperatingSystem | Select  Version | FL #find the windows ver
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
 
 Remove-Item -Path "C:\Windows\Temp\MOZLib_setup_log\step3.txt" -Force 
-Read-Host -Prompt "Done!! Press Enter to exit and run script3"
+#Read-Host -Prompt "Done!! Press Enter to exit and run script3"
