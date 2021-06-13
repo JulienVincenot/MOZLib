100c3a521c2c361f98dc1b2862d20821bb8309da
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-06-13 16:39:25 +0200

    Update script2_2.ps1

--- install_scripts/script2_2.ps1
+++ install_scripts/script2_2.ps1
@@ -2,6 +2,6 @@
 $env:UserName 
 
 #copy the batch file to the "startup" folder
-Copy-Item -Path C:\Windows\Temp\run_setup_part2.bat -Destination "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+Copy-Item -Path C:\Windows\Temp\MOZLib-master\install-scripts\MOZLib_setup_part2.maxpat -Destination "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 
 Remove-Item -Path "C:\Windows\Temp\MOZLib_setup_log\step5.txt" -Force 
