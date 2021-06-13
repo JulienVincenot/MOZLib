0b7ce6882ca3f40b8e46dab749f7b149d826bb5b
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-06-13 20:28:50 +0200

    Update script9.ps1

--- install_scripts/script9.ps1
+++ install_scripts/script9.ps1
@@ -1,1 +1,1 @@
-﻿invoke-expression 'cmd /c start powershell -Command { echo "Creating a new core file" ; wsl chmod +x /mnt/c/Windows/Temp/MOZLib-master/install_scripts/script10.sh ; cd C:\Windows\Temp\MOZLib-master\install_scripts ; bash script10.sh }'
+﻿invoke-expression 'cmd /c start powershell -Command { echo "Creating a new core file" ; wsl chmod +x /mnt/c/Windows/Temp/MOZLib-master/install_scripts/script10.sh ; cd C:\Windows\Temp\MOZLib-master\install_scripts ; bash script10.sh ; Read-Host -Prompt "Done!! Press Enter to exit and run script3" }'
