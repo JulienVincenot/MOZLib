c6070830934258f76beb22182606cc399cdc41be
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-06-13 11:15:12 +0200

    Create script2_2.ps1

---
+++ install_scripts/script2_2.ps1
@@ -0,0 +1,7 @@
+﻿#find the UserName
+$env:UserName 
+
+#copy the batch file to the "startup" folder
+Copy-Item -Path C:\Windows\Temp\run_setup_part2.bat -Destination "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+
+Remove-Item -Path "C:\Windows\Temp\MOZLib_setup_log\step5.txt" -Force 
