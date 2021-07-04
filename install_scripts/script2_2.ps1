4ef5db4fff7d5d1c3f0ebab39618ac8d561c8dbe
marsdrums <80786445+marsdrums@users.noreply.github.com>
2021-07-04 18:00:53 +0200

    changed js destination

--- install_scripts/script2_2.ps1
+++ install_scripts/script2_2.ps1
@@ -4,6 +4,6 @@ $env:UserName
 #copy the batch file to the "startup" folder
 Copy-Item -Path C:\Windows\Temp\MOZLib-master\install_scripts\MOZLib_setup_part2.maxpat -Destination "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 Copy-Item -Path C:\Windows\Temp\MOZLib-master\install_scripts\del_temp_lisp.bat -Destination "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
-Copy-Item -Path C:\Windows\Temp\MOZLib-master\install_scripts\getUserName.js -Destination "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+Copy-Item -Path C:\Windows\Temp\MOZLib-master\install_scripts\getUserName.js -Destination "C:\Windows\Temp\MOZLib_setup_files"
 
 Remove-Item -Path "C:\Windows\Temp\MOZLib_setup_log\step5.txt" -Force 
